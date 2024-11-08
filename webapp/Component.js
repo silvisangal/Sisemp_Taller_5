5e11c1a41b98d661e22954bfa28cffc366623322
Silvana Sandoval Galvis <s.sandovalg@uniandes.edu.co>
2024-11-08 14:20:18 +0000

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("namespace1.productsmanager.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
